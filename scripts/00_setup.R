be16de7516fedbef3f2802f27e95420e21e374a5
Thembinkosi Spogter Phama <phamats.ys@gmail.com>
2026-06-28 13:59:14 +0200

    Initial project structure for churn analysis

---
+++ scripts/00_setup.R
@@ -0,0 +1,6 @@
+# Install required packages
+packages <- c("tidyverse", "lubridate", "janitor", "scales", 
+              "ggplot2", "corrplot", "broom", "survival", 
+              "survminer", "gridExtra", "knitr")
+
+install.packages(packages)
